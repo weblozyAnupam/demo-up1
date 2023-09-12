6d9fc905a416db1da508c4938b64e3ac7470469c
weblozyAnupam <144448295+weblozyAnupam@users.noreply.github.com>
2023-09-12 14:42:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function toggle() {
+  var btn = document.getElementById("btn");
+  btn.style.display = (btn.style.display === "none" || btn.style.display === "") ? "block" : "none";
+}
+toggle(0)
+
+function showTab(tabIndex) {
+  var tabs = document.querySelectorAll(".tab");
+  for (var i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = "none";
+  }
+  tabs[tabIndex].style.display = "block";
+}
+
+// Call showTab(0) to open the first tab by default
+showTab(0);
+
+
+// counter
+
+function updateCounter() {
+  const vernabilityTitles = document.querySelectorAll('.vernability_title');
+  const counterElement = document.getElementById('counter');
+  counterElement.textContent = `${vernabilityTitles.length}`;
+}
+
+// Initial call to update the counter when the page loads
+updateCounter();
+
+
+// color
+
+// Get all elements with class 'nav' and 'li'
+var elements = document.querySelectorAll('.showTab_disp .vernability_title');
+
+// Add a click event listener to each element
+elements.forEach(function(element) {
+    element.addEventListener('click', function() {
+        // Remove the 'active' class from all elements with class 'nav' and 'li'
+        elements.forEach(function(el) {
+            el.classList.remove('active');
+        });
+
+        // Add the 'active' class to the clicked element
+        this.classList.add('active');
+    });
+});
+
+
+
+
+
+
